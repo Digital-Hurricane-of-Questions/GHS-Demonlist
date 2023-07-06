185e9fcd7b96c470c3120320a918a6bd190d0c34
Digital-Hurricane-of-Questions <98976392+Digital-Hurricane-of-Questions@users.noreply.github.com>
2023-07-06 18:03:57 +0300

    bot

--- bot.py
+++ bot.py
@@ -4,9 +4,14 @@ bot = discord.Client()
 
 @client.event
 async def on_message(message):
+    text = message.content
+    if text[:3] = "!ds":
+        text = "!demonlist show" + text[3:]
     if message.author == client.user:
         return
-    if message.content.startswith("!demonlist") or message.content.startswith("!ds"):
-        await message.channel.send(os.system("python demonlist.py" + message.content[message.content.find(" ")+1:]))
+    if text.startswith("!demonlist"):
+        space_point = text.find(" ")+1
+        command = text[space_point: text[space_point+1:].find(" ")]
+        await message.channel.send(os.system("python demonlist.py" + text[text.find(" ")+1:]))
 
 bot.run(os.getenv(token.token))
